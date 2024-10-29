4f834312ef83764b33c96108f558a4bbe3ee23c9
dinogao <dinogao@tencent.com>
2024-10-29 16:32:43 +0800

    update tencentyun to tencentcloud

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-## npm 集成
+## npm integration
 ```shell 
 npm i tls-sig-api-v2
 ```
 
-## 源码集成
-将文件 `TLSSigAPIv2.js` 放置于需要的路径下即可。
+## source code integration
+Just place the file `TLSSigAPIv2.js` in the desired path.
 
-## 接口调用
+## interface call
 ```javascript
 var TLSSigAPIv2 = require('tls-sig-api-v2');
-// var TLSSigAPIv2 = require('./TLSSigAPIv2'); // 源码集成需要使用相对路径
+// var TLSSigAPIv2 = require('./TLSSigAPIv2'); // Source code integration uses relative paths
 
 var api = new TLSSigAPIv2.Api(1400000000, "5bd2850fff3ecb11d7c805251c51ee463a25727bddc2385f3fa8bfee1bb93b5e");
 var sig = api.genSig("xiaojun", 86400*180);
--- TLSSigAPIv2.js
+++ TLSSigAPIv2.js
@@ -41,7 +41,7 @@ var Api = function (sdkappid, key) {
     this.key = key;
 };
 /**
- * 通过传入参数生成 base64 的 hmac 值
+ * Generate the hmac value of base64 by passing in the parameters
  * @param identifier
  * @param currTime
  * @param expire
@@ -59,18 +59,6 @@ Api.prototype._hmacsha256 = function (identifier, currTime, expire, base64UserBu
     const hmac = crypto.createHmac("sha256", this.key);
     return hmac.update(contentToBeSigned).digest('base64');
 };
-/**
- * TRTC业务进房权限加密串需使用用户定义的userbuf
- * @brief 生成 userbuf
- * @param account 用户名
- * @param dwSdkappid sdkappid
- * @param dwAuthID  数字房间号
- * @param dwExpTime 过期时间：该权限加密串的过期时间，建议300秒，实际过期时间:now+dwExpTime
- * @param dwPrivilegeMap 用户权限，255表示所有权限
- * @param dwAccountType 用户类型,默认为0
- * @param roomstr 字符串房间号
- * @return userbuf  {string}  返回的userbuf
- */
 
 /**
  * User-defined userbuf is used for the encrypted string of TRTC service entry permission
@@ -182,16 +170,9 @@ Api.prototype.genSig = function (userid, expire, userBuf) {
     var compressed = zlib.deflateSync(newBuffer(JSON.stringify(sigDoc))).toString('base64');
     return base64url.escape(compressed);
 }
-/**
- *【功能说明】用于签发 TRTC 和 IM 服务中必须要使用的 UserSig 鉴权票据
- *
- *【参数说明】
- * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
- * @param expire - UserSig 票据的过期时间，单位是秒，比如 86400 代表生成的 UserSig 票据在一天后就无法再使用了。
- */
 
 /**
- * Function: Used to issue UserSig that is required by the TRTC and IM services.
+ * Function: Used to issue UserSig that is required by the TRTC and CHAT services.
  *
  * Parameter description:
  * @param userid - User ID. The value can be up to 32 bytes in length and contain letters (a-z and A-Z), digits (0-9), underscores (_), and hyphens (-).
@@ -200,31 +181,6 @@ Api.prototype.genSig = function (userid, expire, userBuf) {
 Api.prototype.genUserSig = function (userid, expire) {
     return this.genSig(userid, expire, null);
 };
-
-/**
- *【功能说明】
- * 用于签发 TRTC 进房参数中可选的 PrivateMapKey 权限票据。
- * PrivateMapKey 需要跟 UserSig 一起使用，但 PrivateMapKey 比 UserSig 有更强的权限控制能力：
- *  - UserSig 只能控制某个 UserID 有无使用 TRTC 服务的权限，只要 UserSig 正确，其对应的 UserID 可以进出任意房间。
- *  - PrivateMapKey 则是将 UserID 的权限控制的更加严格，包括能不能进入某个房间，能不能在该房间里上行音视频等等。
- * 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】=>【应用管理】=>【应用信息】中打开“启动权限密钥”开关。
- *
- *【参数说明】
- * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
- * @param expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
- * @param roomid - 房间号，用于指定该 userid 可以进入的房间号
- * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
- *  - 第 1 位：0000 0001 = 1，创建房间的权限
- *  - 第 2 位：0000 0010 = 2，加入房间的权限
- *  - 第 3 位：0000 0100 = 4，发送语音的权限
- *  - 第 4 位：0000 1000 = 8，接收语音的权限
- *  - 第 5 位：0001 0000 = 16，发送视频的权限  
- *  - 第 6 位：0010 0000 = 32，接收视频的权限  
- *  - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
- *  - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限  
- *  - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
- *  - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
- */
 
 /**
  * Function:
@@ -256,30 +212,6 @@ Api.prototype.genPrivateMapKey = function (userid, expire, roomid, privilegeMap)
     return this.genSig(userid, expire, userBuf);
 
 };
-/**
- *【功能说明】
- * 用于签发 TRTC 进房参数中可选的 PrivateMapKey 权限票据。
- * PrivateMapKey 需要跟 UserSig 一起使用，但 PrivateMapKey 比 UserSig 有更强的权限控制能力：
- *  - UserSig 只能控制某个 UserID 有无使用 TRTC 服务的权限，只要 UserSig 正确，其对应的 UserID 可以进出任意房间。
- *  - PrivateMapKey 则是将 UserID 的权限控制的更加严格，包括能不能进入某个房间，能不能在该房间里上行音视频等等。
- * 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】=>【应用管理】=>【应用信息】中打开“启动权限密钥”开关。
- *
- *【参数说明】
- * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
- * @param expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
- * @param roomstr - 房间号，用于指定该 userid 可以进入的房间号
- * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
- *  - 第 1 位：0000 0001 = 1，创建房间的权限
- *  - 第 2 位：0000 0010 = 2，加入房间的权限
- *  - 第 3 位：0000 0100 = 4，发送语音的权限
- *  - 第 4 位：0000 1000 = 8，接收语音的权限
- *  - 第 5 位：0001 0000 = 16，发送视频的权限  
- *  - 第 6 位：0010 0000 = 32，接收视频的权限  
- *  - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
- *  - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限  
- *  - privilegeMap == 1111 1111 == 255 代表该 userid 在该房间内的所有功能权限。
- *  - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
- */
 
 /**
  * Function:
